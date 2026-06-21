834e64722c6944092ad327c6023e5599e5fd32d4
muzeeb-max <muzeebshaik971@gmail.com>
2026-06-21 14:40:00 +0530

    yotube api error solving 1.2

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -79,10 +79,13 @@ export const executionAPI = {
   }
 };
 
-  export const spotifyAPI = {
-    getLoginUrl: () => api.get('/spotify/login'),
-    getToken: () => api.get('/spotify/token'),
-    disconnect: () => api.post('/spotify/disconnect')
-  };
-
+export const spotifyAPI = {
+  getLoginUrl: () => api.get('/spotify/login'),
+  getToken: () => api.get('/spotify/token'),
+  disconnect: () => api.post('/spotify/disconnect')
+};
+export const musicAPI = {
+  search: (query) =>
+    api.get(`/music/search?q=${encodeURIComponent(query)}`)
+};
 export default api;
